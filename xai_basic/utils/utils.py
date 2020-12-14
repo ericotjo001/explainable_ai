f045abe4caa7e196e45634df2f3fd9b143c1d654
etjoa003 <ericotjoa@gmail.com>
2020-12-14 12:07:31 +0800

    small fixes

--- xai_basic/utils/utils.py
+++ xai_basic/utils/utils.py
@@ -17,20 +17,20 @@ class FastPickleClient(object):
 		self.load_text = 'Loading data via FastPickleClient...'
 	
 	def pickle_data(self, save_data, save_dir, tv=(0,0,None), text=None):
-		if text is None: 
-			save_text = self.save_text
+		if text is not None: 
+			self.save_text = text
 		output = open(save_dir, 'wb')
 		pickle.dump(save_data, output)
 		output.close()		
-		sp.prints('%s\n  %s'%(str(save_text),str(save_dir)), tv=tv)
+		sp.prints('%s\n  %s'%(str(self.save_text),str(save_dir)), tv=tv)
 
 	def load_pickled_data(self, pickled_dir, tv=(0,0,None), text=None):
-		if text is None:
-			load_text = self.load_text
+		if text is not None:
+			self.load_text = text
 		pkl_file = open(pickled_dir, 'rb')
 		this_data = pickle.load(pkl_file)
 		pkl_file.close()		
-		sp.prints('%s\n  %s'%(str(load_text),str(pickled_dir)), tv=tv)
+		sp.prints('%s\n  %s'%(str(self.load_text),str(pickled_dir)), tv=tv)
 		return this_data
 
 fastpickle = FastPickleClient()
